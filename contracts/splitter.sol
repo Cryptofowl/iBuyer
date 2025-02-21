3f1a527690aefacdf752cbc018e73d4ac0e20968
HomicidalChicken <86430321+Cryptofowl@users.noreply.github.com>
2025-02-20 23:30:11 -0500

    Create splitter contract
    Used to encode public inputs from within a contract

---
+++ contracts/splitter.sol
@@ -0,0 +1,22 @@
+// SPDX-License-Identifier: GPLv3
+pragma solidity ^0.8.27;
+
+contract splitter {
+    function split(string memory _input) public pure returns (bytes32[] memory) {
+            bytes memory data = bytes(_input);
+            uint256 length = data.length;
+            bytes32[] memory output = new bytes32[](length);
+            
+            assembly {
+                let dataPtr := add(data, 0x20)
+                let resultPtr := add(output, 0x20)
+                let endPtr := add(dataPtr, mload(data))
+                for {} lt(dataPtr, endPtr) { dataPtr := add(dataPtr, 0x01) resultPtr := add(resultPtr, 0x20) } {
+                    mstore(resultPtr, shr(248, mload(dataPtr)))
+                }
+            }
+            return output;
+        }
+}
+
+    
